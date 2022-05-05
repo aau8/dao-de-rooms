0ba1ee594b9928707ee3e194738ae735e8edd189
Artem <artiom.ygryumov@gmail.com>
2022-05-05 20:56:04 +0300

    -

--- src/js/render.js
+++ src/js/render.js
@@ -1,4 +1,8 @@
 import { removeAll, isWebp } from "./utilities/functions.js"
+import AirDatepicker from "air-datepicker";
+
+new AirDatepicker('#date-search')
+
 
 // Эффект наведения на ссылки в меню
 if (window.innerWidth > 768) {
@@ -39,35 +43,84 @@ if (window.innerWidth > 670) {
         const content = card.querySelector('.se-card__content')
         const title = content.querySelector('.se-card__title')
     
-        content.style.transform = `translateY(calc(100% - ${title.clientHeight}px   ))`
+        content.style.transform = `translateY(calc(100% - ${title.clientHeight - 5}px   ))`
     }
 }
 
 // Плейсхолдер текстовых полей
-// labelTextfield()
-// function labelTextfield() {
-//     const textfieldElems = document.querySelectorAll('.tf')
+labelTextfield()
+function labelTextfield() {
+    const textfieldElems = document.querySelectorAll('.tf')
 
-//     for (let i = 0; i < textfieldElems.length; i++) {
-//         const textfield = textfieldElems[i];
-//         const input = textfield.querySelector('input, textarea')
-//         const label = textfield.querySelector('label')
+    for (let i = 0; i < textfieldElems.length; i++) {
+        const textfield = textfieldElems[i];
+        const input = textfield.querySelector('input, textarea')
+        const label = textfield.querySelector('label')
 
-//         if (input.value != '') {
-//             label.classList.add('_change-label')
-//         }
+        if (input.value != '') {
+            label.classList.add('_change-label')
+        }
 
-//         input.addEventListener('focus', e => {
-//             label.classList.add('_change-label')
-//         })
+        input.addEventListener('focus', e => {
+            label.classList.add('_change-label')
+        })
         
-//         input.addEventListener('blur', e => {
-//             if (input.value === '') {
-//                 label.classList.remove('_change-label')
-//             }
-//         })
-//     }
-// }
+        input.addEventListener('blur', e => {
+            if (input.value === '') {
+                label.classList.remove('_change-label')
+            }
+        })
+    }
+}
+
+// Списки выбора
+select()
+function select() {
+    // Проверяем есть ли выбранные элементы при загрузке страницы. Если есть, то селект заполняется
+    const selectedItemElems = document.querySelectorAll('.select-dropdown__item._selected')
+
+    for (let i = 0; i < selectedItemElems.length; i++) {
+        const selectedItem = selectedItemElems[i];
+        const select = selectedItem.closest('.select')
+        const sTitle = select.querySelector('.select-input__title')
+
+        sTitle.innerText = selectedItem.innerHTML
+        select.classList.add('_valid')
+    }
+
+    // Если пользователь кликнул по селекту, то он открывается/закрывается. Также он закроется если кликнуть вне его области
+    window.addEventListener('click', e => {
+        const target = e.target
+
+        // Если пользователь кликнул вне зоны селекта
+        if (!target.classList.contains('select') && !target.closest('.select._open')) {
+            
+            if (document.querySelector('.select._open')) {
+                document.querySelector('.select._open').classList.remove('_open')
+            }
+        }
+
+        // Если пользователь кликнул по шапке селекта
+        if (target.classList.contains('select-input')) {
+            target.parentElement.classList.toggle('_open')
+        }
+
+        // Если пользователь выбрал пункт из списка селекта
+        if (target.classList.contains('select-dropdown__item')) {
+            const select = target.closest('.select')
+            const sTitle = select.querySelector('.select-input__title')
+            const neighbourTargets = target.parentElement.querySelectorAll('.select-dropdown__item')
+
+            sTitle.innerText = target.innerText
+
+            removeAll(neighbourTargets, '_selected')
+            target.classList.add('_selected')
+
+            select.classList.remove('_open')
+            select.classList.add('_valid')
+        }
+    })
+}
 
 // window.addEventListener('click', e => {
 //     const target = e.target
